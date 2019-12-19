5bb725ad58ecc6a3e874a15450fc91a6efa5de39
Rubén Moreno <rubenmoreno_94@hotmail.com>
2019-12-19 23:59:12 +0100

    Add files via upload

---
+++ Palindromes.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Palindromes {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the string to check: ");
+        String word = input.nextLine();
+        if(isPalindrome(word)){
+            System.out.println("The string "+word+" is palindrome");
+        }
+        else
+        {
+            System.out.println("The string "+word+" is not palindrome");
+        }
+    }
+
+
+    public static boolean isPalindrome(String word){
+        boolean isPalindrome = true;
+        int right = word.length();
+
+        for (int left = 0 ;left < (word.length()); left++){
+            if (word.substring(left, left+1).equalsIgnoreCase(word.substring(right - 1, right)) == false ) {
+                isPalindrome = false;
+                break;
+            }
+            right--;
+        }
+        return isPalindrome;
+    }
+}
